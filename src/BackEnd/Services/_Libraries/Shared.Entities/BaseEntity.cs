036491e73afde5e43a70aef1d9521369082e69f5
admin <admin@bravotechnology.net>
2025-12-15 14:49:16 -0600

    chore (Shared.Entities): - add Shared.Entites project. - add BaseEntity class.
    chore (Shared.Repsotories):
    - add Shared.Repositories project.
    - add project reference to Shared.Entities.
    - add IGenericRepository interface.
    - add GenericRepository class.
    - add EntityFramework Core Postgresql package.

---
+++ src/BackEnd/Services/_Libraries/Shared.Entities/BaseEntity.cs
@@ -0,0 +1,43 @@
+﻿namespace Shared.Entities
+{
+    /// <summary>
+    /// The base entity class that includes common properties for all entities.
+    /// </summary>
+    public class BaseEntity
+    {
+        /// <summary>
+        /// The unique identifier for the entity.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The timestamp when the entity was created.
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// The identifier of the user who created the entity.
+        /// </summary>
+        public Guid CreatedBy { get; set; }
+
+        /// <summary>
+        /// The timestamp when the entity was last updated.
+        /// </summary>
+        public DateTime? UpdatedAt { get; set; }
+
+        /// <summary>
+        /// The identifier of the user who last updated the entity.
+        /// </summary>
+        public Guid? UpdatedBy { get; set; }
+
+        /// <summary>
+        /// The timestamp when the entity was deleted.
+        /// </summary>
+        public DateTime? DeletedAt { get; set; }
+
+        /// <summary>
+        /// The identifier of the user who deleted the entity.
+        /// </summary>
+        public Guid? DeletedBy { get; set; }
+    }
+}
---
+++ src/BackEnd/Services/_Libraries/Shared.Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using Shared.Entities;
+
+namespace Shared.Repositories.Interfaces
+{
+    /// <summary>
+    /// The generic repository interface for CRUD operations
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        /// <summary>
+        /// The method to get entity by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<T> GetByIdAsync(Guid id);
+
+        /// <summary>
+        /// The method to get all entities
+        /// </summary>
+        /// <returns></returns>
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        /// <summary>
+        /// The method to add entity
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<T> AddAsync(T entity);
+
+        /// <summary>
+        /// The method to update entity
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task UpdateAsync(T entity);
+
+        /// <summary>
+        /// The method to delete entity
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task DeleteAsync(T entity);
+    }
+}
