332f5c52f6e838d2953e2333b4a0d9ec05de95f3
admin <admin@bravotechnology.net>
2025-12-16 11:28:53 -0600

    chore(User.Api): - add create user controller and reate user request dto model. - add update user controller and update user request dto model.
    chore(User.Application);
    - add base user request.
    - add cqrs commands for create and update user.
    - add MediatR nuget package.

---
+++ src/BackEnd/Services/CrmSaSS/Api/User.Api/Models/UpdateUserRequestDto.cs
@@ -0,0 +1,20 @@
+﻿namespace User.Api.Models
+{
+    public class UpdateUserRequestDto
+    {
+        /// <summary>
+        /// The first name of the user.
+        /// </summary>
+        public string? FirstName { get; set; }
+
+        /// <summary>
+        /// The last name of the user.
+        /// </summary>
+        public string? LastName { get; set; }
+
+        /// <summary>
+        /// The email address of the user.
+        /// </summary>
+        public string? Email { get; set; }
+    }
+}
---
+++ src/BackEnd/Services/CrmSaSS/Core/User.Application/Features/User/Commands/CreateUser/CreateUserRequest.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using User.Application.Features.User._Share;
+
+namespace User.Application.Features.User.Commands.CreateUser
+{
+    public record CreateUserRequest(BaseUserRequest request) : IRequest<Guid>;
+}
