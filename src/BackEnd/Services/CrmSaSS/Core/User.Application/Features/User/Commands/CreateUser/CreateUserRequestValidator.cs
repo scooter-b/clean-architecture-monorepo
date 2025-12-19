1f0a371a7a6254762cead3b82cea1bcc49bdfbfb
admin <admin@bravotechnology.net>
2025-12-19 12:42:03 -0600

    chore: removed obsolete projects

--- src/BackEnd/Services/CrmSaSS/Core/User.Application/Features/User/Commands/CreateUser/CreateUserRequestValidator.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace User.Application.Features.User.Commands.CreateUser
-{
-    public class CreateUserRequestValidator
-    {
-        // TODO: Implement validation logic as necessary
-    }
-}
